132c27e7cc90132252612f051ceadb81b154ecae
Alex Vidal <58011097+Alexvidalcor@users.noreply.github.com>
2021-02-02 02:34:52 +0100

    Validation added
    Token management optional

--- ConfigAWS.py
+++ ConfigAWS.py
@@ -1,5 +1,17 @@
+import subprocess
 from pathlib import Path
 import getpass as gp
+import boto3
+from botocore.exceptions import ClientError
+
+def ValidCredentials():
+    try:
+        sts = boto3.client('sts')
+        sts.get_caller_identity()
+        return "Valid"
+    except ClientError:
+        print("Credenciales no válidas")
+        return "Invalid"
 
 def CheckDirectories():
     mainUser = gp.getuser()
@@ -10,32 +22,32 @@ def CheckDirectories():
         Path(f"/home/{mainUser}/.aws").mkdir(parents=True, exist_ok=True)
         print("OK")
 
-def CreateCredentials(keyID, accessID):
+def CreateCredentials(accessID, keyID, tokenID):
     try:
         f = open(f"/home/{gp.getuser()}/.aws/credentials", "x+")
-        print("Escribiendo nuevas credenciales...")
+        print("Escribiendo credenciales...")
         f.write("[default]\n")
-        f.write(f"aws_access_key_id = {keyID}\n")
-        f.write(f"aws_secret_access_key = {accessID}\n")
+        f.write(f"aws_access_key_id={accessID}\n")
+        f.write(f"aws_secret_access_key={keyID}\n")
+        if tokenID != "":
+            f.write(f"aws_session_token={tokenID}\n")
         f.close
         print("OK")
-
     except FileExistsError:
         print("Modificando viejas credenciales...")
         Path(f"/home/{gp.getuser()}/.aws/credentials").unlink()
-        f = open(f"/home/{gp.getuser()}/.aws/credentials", "x+")
-        f.write("[default]\n")
-        f.write(f"aws_access_key_id = {keyID}\n")
-        f.write(f"aws_secret_access_key = {accessID}\n")
-        f.close
-        print("OK")
+        CreateCredentials(accessID, keyID, tokenID)
 
 def AskCredentials():
-    keyPass = gp.getpass("Introduce key: ")
     accessPass = gp.getpass("Introduce access: ")
-    return keyPass, accessPass
+    keyPass = gp.getpass("Introduce key: ")
+    tokenPass = gp.getpass("Introduce tu token (opcional):")
+    return accessPass, keyPass, tokenPass
 
 if __name__ == "__main__":
-    CheckDirectories()
-    keyPass, accessPass = AskCredentials()
-    CreateCredentials(keyPass, accessPass)
+    createdCheck = CheckDirectories()
+    accessPass, keyPass, tokenPass = AskCredentials()
+    if keyPass != "" and accessPass != "":
+        CreateCredentials(accessPass, keyPass, tokenPass)
+    else:
+        print("Credenciales vacías o incompletas. Programa terminado")
