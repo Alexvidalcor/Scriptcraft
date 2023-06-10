cfa5c19248a84b14a198b41498d4e48a61dfa2f7
Alex Vidal <58011097+Alexvidalcor@users.noreply.github.com>
2023-06-10 11:55:03 +0200

    Create Vscode-wayland.py
    script to waylandize vscode

---
+++ Vscode-wayland.py
@@ -0,0 +1,39 @@
+# Native libraries
+import os
+from os.path import expanduser
+
+# General variables
+codePath = ".local/share/applications"
+codeFiles = ["code.desktop", "code-url-handler.desktop"]
+xorgLine = "Exec=/usr/share/code/code"
+waylandLine = "Exec=/usr/share/code/code --ozone-platform-hint=auto"
+
+
+# Logic
+
+print("-------\nStarting script - Wayland in vscode\n -------")
+
+if codePath.startswith(".local"):
+    print("Codepath in home")
+    homeUser = expanduser("~")
+    print("-------")
+
+print(f"Path used: {homeUser}/{codePath}\n -------")
+
+for file in os.listdir(f"{homeUser}/{codePath}"):
+    if file in codeFiles:
+
+        print(f"File detected: {file}")
+        
+        with open(f"{homeUser}/{codePath}/{file}", "rt") as f:
+            s = f.read()
+
+        if waylandLine not in s:       
+            with open(f"{homeUser}/{codePath}/{file}", "wt") as f:
+                f.write(s.replace(xorgLine, waylandLine))
+                print(f"File modified: {file}\n-------")
+        else:
+            print(f"File not modified: {file}\n-------")
+
+    
+print("Finished, please reboot the computer\n -------")
