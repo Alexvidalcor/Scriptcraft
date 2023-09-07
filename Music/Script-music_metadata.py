3a7c41131a6825f3c01fba757bb2796f23ea1118
alexvidalcor <correovidal@outlook.com>
2023-09-07 19:18:54 +0000

    initial music script added

--- Music/Script-music_metadata.py
+++ Music/Script-music_metadata.py
@@ -1,18 +1,65 @@
+# native libraries
+import os
+
+# third party libraries
 import mutagen
-import sqlite3
-
-a = mutagen.File("",easy=True)
-
-fileMusic = ""
-fileSplitter = ""[:-4].split(" - ")
-formatDetector = fileMusic[-4:]
+import pandas as pd
 
 
-a.delete()
-a.saver()
+# main vars
+musicPath = "test"
 
-a.add_tags()
-a.tags["artist"]=""
-a.tags["title"]=""
+# functions
+def fileModifier(filePath):
+    '''
+    Modify metadata
+    return music file with metadata modified
+    '''
+    try:
+        fileName = filePath.split("/")[-1]
+        fileSplitter = fileName[:-4].split(" - ")
 
-print(a.tags)
+        fileArtist = fileSplitter[0]
+        fileTitle = fileSplitter[1]
+        fileFormat = fileName[-4:]
+
+        df.loc[len(df)] = [fileArtist, fileTitle, fileFormat, "OK"]
+
+    except IndexError:
+        df.loc[len(df)] = ["NO", "NO", "NO", fileSplitter]
+
+# execution
+df = pd.DataFrame(columns=["Artist","Title","Format","Result"])
+
+for currentDir, subFolders, files in os.walk(musicPath):
+    for file in files:
+        filePath = os.path.join(currentDir, file)
+        fileModifier(filePath)
+
+df.to_csv("test.csv", index=False)
+    
+
+
+
+
+# a = mutagen.File("",easy=True)
+
+# fileMusic = ""
+# fileSplitter = ""[:-4].split(" - ")
+# formatDetector = fileMusic[-4:]
+
+
+# a.delete()
+# a.saver()
+
+# a.add_tags()
+# a.tags["artist"]=""
+# a.tags["title"]=""
+
+# print(a.tags)
+
+
+
+
+# # a.delete()
+# # a.saver()
