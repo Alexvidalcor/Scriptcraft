f381f55a41b26d6a1e0edfdccb6f23866f046bec
Alex Vidal <58011097+Alexvidalcor@users.noreply.github.com>
2024-05-03 00:38:39 +0200

    added new echos

--- Vscode/Vscode-compose_Continue-ollamastart.sh
+++ Vscode/Vscode-compose_Continue-ollamastart.sh
@@ -2,16 +2,20 @@
 
 set -e
 
-echo "Starting services..."
+echo "Starting net..."
 docker-compose -f Vscode-compose_Continue-ollama.yaml -p Vscode-compose_Continue-ollama up -d || { echo "Failed to start services"; exit 1; }
+echo "------------------------------"
 
 echo "Running GPT command..."
-docker exec Vscode-cont_Continue-gpt ollama run codellama:7b-instruct || { echo "Failed to execute on GPT container"; exit 1; }
+docker exec Vscode-cont_Continue-gpt ollama run codellama:7b || { echo "Failed to execute on GPT container"; exit 1; }
+echo "------------------------------"
 
 echo "Running Autocomplete command..."
-docker exec Vscode-cont_Continue-autocomplete ollama run ollama run starcoder2:3b || { echo "Failed to execute on Autocomplete container"; exit 1; }
+docker exec Vscode-cont_Continue-autocomplete ollama run starcoder2:3b || { echo "Failed to execute on Autocomplete container"; exit 1; }
+echo "------------------------------"
 
 echo "Running Embeddings command..."
-docker exec Vscode-cont_Continue-embeddings ollama run codellama:7b-instruct || { echo "Failed to execute on Embeddings container"; exit 1; }
+docker exec Vscode-cont_Continue-embeddings ollama run codellama:7b || { echo "Failed to execute on Embeddings container"; exit 1; }
+echo "------------------------------"
 
 echo "All services started and commands executed successfully."
